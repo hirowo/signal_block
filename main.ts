117a912aad4be571c72c15a3d16e757a7be7939e
hirowo <34668037+hirowo@users.noreply.github.com>
2020-05-25 17:06:00 +0900

    change icon

---
+++ main.ts
@@ -0,0 +1,6 @@
+//% weight=70 icon="\uf075" color=#555555 block="コメント"
+namespace comment {
+    //% blockId=show_strings block="コメント %v"
+    export function noaction(text: string): void {
+    }
+}
