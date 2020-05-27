95dbf78c7f4b1b0f090baba1cdb91614821ddb99
hirowo <34668037+hirowo@users.noreply.github.com>
2020-05-27 14:40:14 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,19 +1,29 @@
 //% weight=70 icon="\uf075" color=#555555 block="コメント"
 namespace comment {
+    let p1 =DigitalPin.P0;  
+    let p2 = DigitalPin.P16;  
+
+    //% blockId=INIT block="ピン指定 %v"
+    export function init(PIN1: DigitalPin, PIN2: DigitalPin): void {
+        p1 = PIN1;
+        p2 = PIN2;
+
+    }
+    
     //% blockId=show_strings block="赤を点灯 %v"
     export function aka(): void {
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(p1, 0)
+        pins.digitalWritePin(p2, 1)
     }
     //% blockId=sono2 block="黄色を点灯 %v"
     export function kiiro(): void {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(p1, 1)
+        pins.digitalWritePin(p2, 0)
     }
     //% blockId=sono2 block="黄色を点灯 %v"
     export function midori(): void {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(p1, 1)
+        pins.digitalWritePin(p2, 1)
     }
 
 }
