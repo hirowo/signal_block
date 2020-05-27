3da53fd41821e6e37d674ccafe28385ddf5c2370
hirowo <34668037+hirowo@users.noreply.github.com>
2020-05-27 14:41:52 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ namespace comment {
     let p1 =DigitalPin.P0;  
     let p2 = DigitalPin.P16;  
 
-    //% blockId=INIT block="ピン指定 %v"
+    //% blockId=INIT block="ピン指定 %PIN1 %PIN2 %v"
     export function init(PIN1: DigitalPin, PIN2: DigitalPin): void {
         p1 = PIN1;
         p2 = PIN2;
