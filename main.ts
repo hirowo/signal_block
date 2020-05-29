4315da42985801e19f3d362e6c63abe2b2683210
hirowo <34668037+hirowo@users.noreply.github.com>
2020-05-29 16:44:32 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -2,12 +2,16 @@
 namespace comment {
     let p1 = DigitalPin.P0;
     let p2 = DigitalPin.P16;
-
+    export class SIGNAL_LIHGHT {
+        pin1: DigitalPin;
+        pin2: DigitalPin;        
+    }
     //% blockId=INIT block="ピン指定 %PIN1 %PIN2 %v"
-    export function init(PIN1: DigitalPin, PIN2: DigitalPin){
-        p1 = PIN1;
-        p2 = PIN2;
-        return p1
+    export function init(PIN1: DigitalPin, PIN2: DigitalPin) :SIGNAL_LIHGHT{
+        let  block= new SIGNAL_LIHGHT();
+        block.pin1 = PIN1;
+        block.pin2 = PIN2;
+        return block
     }
     //% block="前%v"
     
