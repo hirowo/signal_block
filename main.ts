29fba2393a1f5cc0afdb7dfd5e01ebffa562bbe8
hirowo <34668037+hirowo@users.noreply.github.com>
2020-05-29 17:04:40 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -6,40 +6,34 @@ namespace comment {
         pin1: DigitalPin;
         pin2: DigitalPin;        
     }
+    
     //% blockId=INIT block="ピン指定 %PIN1 %PIN2 %v"
     export function init(PIN1: DigitalPin, PIN2: DigitalPin) :SIGNAL_LIHGHT{
-        let  block= new SIGNAL_LIHGHT();
+        let block = new SIGNAL_LIHGHT();
         block.pin1 = PIN1;
         block.pin2 = PIN2;
         return block
     }
-    //% block="前%v"
-    
-    export function Forward() {
-
-        return 1
-    }
-
-
+  
     //% blockId=show_strings block="赤を点灯 %v"
-    export function aka(): void {
-        pins.digitalWritePin(p1, 0)
-        pins.digitalWritePin(p2, 1)
+    export function aka(ASAIN: SIGNAL_LIHGHT): void {
+        pins.digitalWritePin(ASAIN.pin1, 0)
+        pins.digitalWritePin(ASAIN.pin2, 1)
     }
     //% blockId=sono2 block="黄色を点灯 %v"
-    export function kiiro(): void {
-        pins.digitalWritePin(p1, 1)
-        pins.digitalWritePin(p2, 0)
+    export function kiiro(ASAIN: SIGNAL_LIHGHT): void {
+        pins.digitalWritePin(ASAIN.pin1, 1)
+        pins.digitalWritePin(ASAIN.pin1, 0)
     }
     //% blockId=sono3 block="緑を点灯 %v"
-    export function midori(): void {
-        pins.digitalWritePin(p1, 0)
-        pins.digitalWritePin(p2, 0)
+    export function midori(ASAIN: SIGNAL_LIHGHT): void {
+        pins.digitalWritePin(ASAIN.pin1, 0)
+        pins.digitalWritePin(ASAIN.pin1, 0)
     }
     //% blockId=sono4 block="全部消す %v"
-    export function kesu(): void {
-        pins.digitalWritePin(p1, 1)
-        pins.digitalWritePin(p2, 1)
+    export function kesu(ASAIN: SIGNAL_LIHGHT): void {
+        pins.digitalWritePin(ASAIN.pin1, 1)
+        pins.digitalWritePin(ASAIN.pin1, 1)
     }
 
 }
