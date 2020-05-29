63c4bef0a7e4ae2ba85213d383d18d560c76f8dd
hirowo <34668037+hirowo@users.noreply.github.com>
2020-05-29 16:16:41 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
 //% weight=70 icon="\uf075" color=#0000FF block="信号機"
 namespace comment {
-    let p1 =DigitalPin.P0;  
-    let p2 = DigitalPin.P16;  
+    let p1 = DigitalPin.P0;
+    let p2 = DigitalPin.P16;
 
     //% blockId=INIT block="ピン指定 %PIN1 %PIN2 %v"
-    export function init(PIN1: DigitalPin, PIN2: DigitalPin): void {
+    export function init(PIN1: DigitalPin, PIN2: DigitalPin) {
         p1 = PIN1;
         p2 = PIN2;
 
+        return p1
     }
-    
+
     //% blockId=show_strings block="赤を点灯 %v"
     export function aka(): void {
         pins.digitalWritePin(p1, 0)
@@ -30,7 +31,6 @@ namespace comment {
         pins.digitalWritePin(p1, 1)
         pins.digitalWritePin(p2, 1)
     }
-    
 
 }
 
